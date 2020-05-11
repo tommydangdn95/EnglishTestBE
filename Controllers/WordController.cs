0a519b0c8323ccede36590377bb6f848e6955cd8
Tran Thuy <thuy.dang@getz.co>
2020-05-11 22:53:49 +0700

    fix list

--- Controllers/WordController.cs
+++ Controllers/WordController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RandomShowEnglish.Helper;
+using RandomShowEnglish.Model;
 
 namespace RandomShowEnglish.Controllers
 {
@@ -22,85 +24,69 @@ namespace RandomShowEnglish.Controllers
         public IActionResult ShowRandomWord()
         {
             var list = InitList();
-            return Ok(GetRandomWord(list));
+            var randomWord = GetRandomWord(list);
+            var words = new Words
+            {
+                MainWord = randomWord.Key,
+                SynonymWord = randomWord.Value
+            };
+            return Ok(words);
         }
 
-        private List<string> InitList()
+        private Dictionary<string, string> InitList()
         {
-            var listWord = new List<string>()
+            var listWord = new Dictionary<string, string>()
             {
-                "actor",
-                "performer",
-                "artist",
-                "virtuoso",
-                "atmosphere",
-                "audience",
-                "ballet",
-                "classical",
-                "concert",
-                "show",
-                "costume",
-                "clothing",
-                "craft",
-                "handicraft",
-                "design",
-                "create",
-                "dramatic",
-                "emotion",
-                "feeling",
-                "escape",
-                "get away",
-                "exhibition",
-                "display",
-                "festival",
-                "fete",
-                "gallery",
-                "image",
-                "influence",
-                "impact",
-                "landscape",
-                "live",
-                "lyric",
-                "word",
-                "musical",
-                "musician",
-                "artist",
-                "painting",
-                "drawing",
-                "play",
-                "drama",
-                "poetry",
-                "rhyme",
-                "popular",
-                "famous",
-                "relaxing",
-                "calm",
-                "style",
-                "manner",
-                "taste",
-                "Celebrate",
-                "Celebration",
-                "Celebratory",
-                "Masquerade",
-                "Parade",
-                "Massive",
-                "Tradition",
-                "early Februry",
-                "crowded point",
-                "turning moment",
-                "Reunion",
-                "take the chance",
-                "Fantastic",
-                "dress up"
+                {"actor", "performer"},
+                {"artist", "virtuoso" },
+                {"atmosphere",  "không khí"},
+                {"audience", "spectator" },
+                {"ballet", "ba lê"},
+                {"classical", "classic" },
+                {"concert", "show" },
+                {"costume", "clothing" },
+                {"craft", "handicraft" },
+                {"design", "create" },
+                {"dramatic", "(thuộc) kịch/ như kịch" },
+                {"emotion", "feeling"},
+                {"escape", "get away" },
+                {"exhibition", "display" },
+                {"festival", "fete" },
+                {"gallery", "phòng trưng bày tranh tượng" },
+                {"image", "hình ảnh" },
+                {"influence", "impact" },
+                {"landscape", "tranh phong cảnh" },
+                {"live", "sống, trực tiếp"},
+                {"lyric", "word" },
+                {"musical", "lời bài hát" },
+                {"musician", "artist" },
+                {"painting", "drawing" },
+                {"play", "drama" },
+                {"poetry", "rhyme" },
+                {"popular", "famous" },
+                {"relaxing", "calm" },
+                {"style", "manner" },
+                {"taste", "khiếu thẩm mĩ" },
+                {"Celebrate", "Celebration" },
+                {"Masquerade", "Masquerade party" },
+                {"Parade", "cuộc diễu hành"},
+                {"Massive", "huge" },
+                {"Tradition", "Traditional" },
+                {"early Februry", "đàu tháng hai" },
+                {"crowded point", "tụ điểm" },
+                {"turning moment", "khoảnh khắc giao thừa" },
+                {"Reunion", "họp mặt, đoàn tụ" },
+                {"take the chance", "nhân cơ hội" },
+                {"Fantastic", "wonderful" },
+                {"dress up", "ăn diện lên" }
             };
 
             return listWord;
         }
 
-        private string GetRandomWord(List<string> listWord)
+        private KeyValuePair<string, string> GetRandomWord(Dictionary<string, string> listWord)
         {
-            int index = rand.GetRandIns().Next(listWord.Count);
-            return listWord[index];
+            return listWord.ElementAt(rand.GetRandIns().Next(listWord.Count));
         }
     }
 }
---
+++ Model/Words.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomShowEnglish.Model
+{
+    public class Words
+    {
+        public string MainWord { get; set; }
+        public string SynonymWord { get; set; }
+    }
+}
